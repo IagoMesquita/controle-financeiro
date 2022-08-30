c1f6d43d56ee462ad58bdbe877929d4f3f61566a
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-30 14:41:50 -0300

    styles: Header and Resume

---
+++ components/Header/styles.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Contaneiner =  styled.div`
+  height: 150px;
+  text-align: center;
+  background-color: teal;
+`
+export const Header = styled.header`
+  padding: 20px;
+  color: #fff;
+`
+export const Title = styled.h2``;
--- styles/globals.tsx
+++ styles/globals.tsx
@@ -6,10 +6,14 @@ export const GlobalStyle = createGlobalStyle`
   padding: 0;
   margin: 0;
   box-sizing: border-box;
-  background: #f2f2f2;
-  font-family: 'Poppins', sans-serif;
+  
 }
 
+body {
+  background: black;
+  /* background: #f2f2f2; */
+  font-family: 'Poppins', sans-serif;
+}
 `
 
 
