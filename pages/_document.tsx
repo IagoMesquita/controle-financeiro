7fc52e6303003dcf0d5929c1bc740ccc004631da
Iago Mesquita <iagomesquitaalves@gmail.com>
2022-08-29 21:09:18 -0300

    chore: _document html

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,8 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, { DocumentContext, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
+
   static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet()
     const originalRenderPage = ctx.renderPage
@@ -22,4 +23,21 @@ export default class MyDocument extends Document {
       sheet.seal()
     }
   }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />            
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+
 }
